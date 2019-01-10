db29b6fa35632e72423f7d7caf68b79cfa881fac
doulos76 <inojyes@gmail.com>
2019-01-10 12:35:21 +0900

    add plist

---
+++ practice/PropertyListPractice/PropertyListPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PropertyListPractice
+//
+//  Created by dave76 on 10/01/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
